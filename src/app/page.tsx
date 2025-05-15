4cfaeb0c7655196c6e83ce3874c21913e83d1def
ajay-desai9 <ajay.desai509@gmail.com>
2025-05-15 13:01:05 -0400

    sign in/out buttons, implemented protected pages without authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import { SignInButton } from '@clerk/nextjs'
+"use client";
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
-      <SignInButton />
+      HomePage
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
     </div>
   )
 }
